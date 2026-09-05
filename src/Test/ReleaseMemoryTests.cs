7cc761bacff5e20d2caa0b8a07289674223ded8b
MataM15 <pablorodrigo14@gmail.com>
2026-09-05 19:41:56 +0000

    test: avoid release fixture namespace collision

--- src/Test/ReleaseMemoryTests.cs
+++ src/Test/ReleaseMemoryTests.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
-namespace WinMemoryCleaner.ReleaseMemoryTests
+namespace WinMemoryCleaner.MemoryReleaseTests
 {
     [TestFixture]
     public sealed class ReleaseMemoryTests
